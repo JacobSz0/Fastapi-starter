9a36221b8a4a5973fc8134c8c0ef516429f51e3d
Jacob Sullenszino <JacobSz@mail.com>
2023-11-03 13:36:19 -0700

    fixed return objects

--- app.py
+++ app.py
@@ -1,72 +1,76 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import sqlite3
+import json
 
 def create_connection():
-  connection = sqlite3.connect("books.db")
-  return connection
+    connection = sqlite3.connect("books.db")
+    return connection
 
 def create_table():
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS books (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- title TEXT NOT NULL,
- author TEXT NOT NULL
- )
- """)
- connection.commit()
- connection.close()
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        book_data TEXT NOT NULL
+    )
+    """)
+    connection.commit()
+    connection.close()
 
-create_table() # Call this function to create the table
+create_table()  # Call this function to create the table
 
 class BookCreate(BaseModel):
-  title: str
-  author: str
+    title: str
+    author: str
 
 class Book(BookCreate):
-  id: int
+    id: int
 
 def get_all_books():
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books")
+    cursor.execute("SELECT id, book_data FROM books")
     books = cursor.fetchall()
     connection.close()
-    return books
+
+    books_list = []
+    for book in books:
+        book_dict = {"id": book[0], **json.loads(book[1])}
+        books_list.append(book_dict)
+
+    return books_list
 
 def create_book(book: BookCreate):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (book.title, book.author))
- connection.commit()
- connection.close()
- book_id = cursor.lastrowid
-
- connection.close()
- return book_id
+    connection = create_connection()
+    cursor = connection.cursor()
+    book_data = json.dumps({"title": book.title, "author": book.author})
+    cursor.execute("INSERT INTO books (book_data) VALUES (?)", (book_data,))
+    connection.commit()
+    book_id = cursor.lastrowid
+    connection.close()
+    return book_id
 
 def get_book_by_id(book_id: int):
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books WHERE id = ?", (book_id,))
+    cursor.execute("SELECT book_data FROM books WHERE id = ?", (book_id,))
     book = cursor.fetchone()
     connection.close()
     if book:
-        return book
+        return json.loads(book[0])
     else:
         raise HTTPException(status_code=404, detail="Book not found")
 
-# Function to update a book by its ID
 def update_book(book_id: int, book: BookCreate):
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("UPDATE books SET title = ?, author = ? WHERE id = ?", (book.title, book.author, book_id))
+    book_data = json.dumps({"title": book.title, "author": book.author})
+    cursor.execute("UPDATE books SET book_data = ? WHERE id = ?", (book_data, book_id))
     connection.commit()
     connection.close()
 
-# Function to delete a book by its ID
 def delete_book(book_id: int):
     connection = create_connection()
     cursor = connection.cursor()
@@ -74,12 +78,11 @@ def delete_book(book_id: int):
     connection.commit()
     connection.close()
 
-
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return {"message": "Welcome to the CRUD API"}
+    return {"message": "Welcome to the CRUD API"}
 
 @app.get("/books/")
 def get_all_books_endpoint():
@@ -88,25 +91,22 @@ def get_all_books_endpoint():
 
 @app.post("/books/")
 def create_book_endpoint(book: BookCreate):
- book_id = create_book(book)
- return {"id": book_id, **book.model_dump()}
+    book_id = create_book(book)
+    return {"id": book_id, "title": book.title, "author": book.author}
 
-# Endpoint to get a single book by its ID
 @app.get("/books/{book_id}")
 def get_book(book_id: int):
     book = get_book_by_id(book_id)
     return {"book": book}
 
-# Endpoint to update a book by its ID
 @app.put("/books/{book_id}")
 def update_book_endpoint(book_id: int, book: BookCreate):
-    get_book_by_id(book_id)  # Check if the book exists, raises 404 if not found
+    get_book_by_id(book_id)
     update_book(book_id, book)
-    return {"book": book, "id":book_id}
+    return {"message": "Book updated successfully", "id": book_id, "content": book}
 
-# Endpoint to delete a book by its ID
 @app.delete("/books/{book_id}")
 def delete_book_endpoint(book_id: int):
-    get_book_by_id(book_id)  # Check if the book exists, raises 404 if not found
+    get_book_by_id(book_id)
     delete_book(book_id)
     return {"message": "Book deleted successfully"}
